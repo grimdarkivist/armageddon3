9adb21b69d49ef19c31c90bb147e74dca2f2c48f
grimdarkivist <grimdarkivist@proton.me>
2026-01-01 11:28:21 +1100

    Version 1.0

---
+++ README.md
@@ -0,0 +1,9 @@
+# www.armageddon3.com
+
+##Notes
+file.ext = main file
+file.timestamp.ext = different versions of the file, including one for the main file.
+
+##Tools
+Websitino - https://trikko.github.io/websitino/, https://github.com/trikko/websitino
+JPEXS Free Flash Decompiler - https://github.com/jindrapetrik/jpexs-decompiler
---
+++ scripts/reporting/login.pl
@@ -0,0 +1,50 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2//EN">
+<HTML>
+<HEAD>
+   <TITLE>PASSWORD ERROR!</TITLE>
+</HEAD>
+
+<BODY topmargin="0" leftmargin="0" marginwidth="0" marginheight="0" text ="#FFFFFF" BGCOLOR="#000000" background="/images/nh456c-left-2_02.jpg">
+
+
+<TABLE BORDER=0 CELLPADDING=0 CELLSPACING=0>
+<TR>
+<TD valign=top><img src="/images/nh456c-left-2_01.jpg" width=50 height=138 border=0></TD>
+<TD><img src="/images/invisible.gif" width=10 height=1 border=0></TD>
+<TD><img src="/English/Campaign/images/campaignlogo.gif" width=670 height=117 border=0><BR CLEAR><TABLE WIDTH="650" ALIGN=CENTER CELLSPACING=0 CELLPADDING=0 BORDER="0" VALIGN=TOP><TR><TD WIDTH="650"><CENTER><P><FONT SIZE="-1"> </FONT><FONT SIZE="+2" COLOR="#FFOOOO"><B>Password Error! Access Denied!!</B></FONT><FONT SIZE="-1"> </FONT></P></CENTER>
+
+<p>It seems that your password or username have been entered incorrectly. Please try again. If the problem persists contact us at <A HREF="mailto:techpriest@games-workshop.com">techpriest@Games-Workshop.com</A> and we'll endeavor to get it fixed as quick as possible. If you haven't registered for the <B>Third War for Armageddon Campaign</B>, proceed to the <A HREF="http://www.armageddon3.com/English/Campaign/registration/registration1.html">Registration Area</A> in order to choose a Warzone and start defending or destroying Armageddon!</p>
+
+
+
+<Form ACTION=login.pl METHOD=POST>
+
+<TABLE border=0 cellPadding=2 cellSpacing=0 ALIGN=CENTER>
+  <TBODY>
+  <TR>
+    <TD align=middle bgColor=#000000 colSpan=2><FONT color=#ffffff 
+      face="helvetica, arial, sans-serif"><B>&nbsp; ORK SENTRY</B></FONT> 
+  </TD></TR>
+  <TR>
+    <TD><FONT face="helvetica, arial, sans-serif"><B>Who you? (Email address):</B></FONT> </TD></TR>
+  <TR>
+    <TD><INPUT name=UID> </TD></TR>
+  <TR>
+    <TD><FONT face="helvetica, arial, sans-serif"><B>What's da password?:</B></FONT> </TD></TR>
+  <TR>
+    <TD><INPUT name=PWD TYPE=PASSWORD> </TD></TR>
+ <TR>
+    <TD align=middle colSpan=2>&nbsp;<BR><INPUT type=submit value="Let me in!!!"> 
+  </TD></TR>
+  <TR>
+  </TBODY></TABLE>
+<INPUT TYPE=HIDDEN NAME=form VALUE=orkstep2.html>
+</FORM>
+
+
+</TD>
+</TR>
+</TABLE></TD></TR></TABLE><BR CLEAR><TABLE WIDTH=668 ALIGN=CENTER CELLSPACING=0 CELLPADDING=0 VALIGN=TOP BORDER=0><TR><TD WIDTH="120" ALIGN=CENTER><A HREF="/English/40KIntro/40Kintro.html"><IMG SRC="/Images/40Kbutton.gif" HEIGHT=24 WIDTH="120" BORDER=0></A></TD><TD WIDTH="17" ALIGN=CENTER><IMG SRC="/Images/invisiblenav.gif" HEIGHT=17 WIDTH="17"></TD><TD WIDTH="120" ALIGN=CENTER><A HREF="/English/Campaign/campaign_map.html"><IMG SRC="/Images/campaignbutton.gif" HEIGHT=24 WIDTH="120" BORDER=0></A></TD><TD WIDTH="17" ALIGN=CENTER><IMG SRC="/Images/invisiblenav.gif" HEIGHT=17 WIDTH="17"></TD><TD WIDTH="120" ALIGN=CENTER><A HREF="/English/Codex/codexarm.html"><IMG SRC="/Images/codexbutton.gif" HEIGHT=24 WIDTH="120" BORDER=0></A></TD><TD WIDTH="17" ALIGN=CENTER><IMG SRC="/Images/invisiblenav.gif" HEIGHT=17 WIDTH="17"></TD><TD WIDTH="120" ALIGN=CENTER><A HREF="/English/History/history.html"><IMG SRC="/Images/historybutton.gif" HEIGHT=24 WIDTH="120" BORDER=0></A></TD><TD WIDTH="17" ALIGN=CENTER><IMG SRC="/Images/invisiblenav.gif" HEIGHT=17 WIDTH="17"></TD><TD WIDTH="120" ALIGN=CENTER><A HREF="/English/News/news.html"><IMG SRC="/Images/newsbutton.gif" HEIGHT=24 WIDTH="120" BORDER=0></A></TD></TR></TABLE>
+
+</BODY>
+</HTML>
